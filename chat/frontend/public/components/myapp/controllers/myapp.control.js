374abe4a402195ab49825d525d1dfa7657321b7f
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-07-06 03:05:23 +0530

    Added init method for controller entry

--- chat/frontend/public/components/myapp/controllers/myapp.control.js
+++ chat/frontend/public/components/myapp/controllers/myapp.control.js
@@ -1,97 +1,93 @@
 myapp
-    .controller('MessageController', ["$scope", "$window",
-    	"$stateParams", "$localStorage", "Message", "Upload",
-    	function($scope, $window, $stateParams, $localStorage, Message, Upload) {
-	        if (!('token' in $localStorage)) {
-    			$window.location.href = '/#!/login';
-    		}
+  .controller('MessageController', ["$scope", "$window", "$stateParams", "$localStorage", "Message", "Upload",
+    function($scope, $window, $stateParams, $localStorage, Message, Upload) {
+      if (!('token' in $localStorage)) {
+        $window.location.href = '/#!/login';
+      }
 
-    		$scope.send_to = $stateParams.username;
-    		$scope.text = '';
+      $scope.send_to = $stateParams.username;
+      $scope.text = '';
 
-    		Message.query({username:$scope.send_to}).$promise.then(
-    			function(messages) {
-    				for (var i=0; i < messages.length; i++) {
-    					var fname = messages[i].ufile_name;
-    					var ext = fname.substring(
-    						fname.lastIndexOf('.')+1, fname.length
-    						) || fname;
+      $scope.init = function() {
+        Message.query({username:$scope.send_to}).$promise.then(
+          function(messages) {
+            for (var i=0; i < messages.length; i++) {
+              var fname = messages[i].ufile_name;
+              var ext = fname.substring(fname.lastIndexOf('.')+1, fname.length) || fname;
 
-    					var if_img = false;
-    					var img_types = ['png', 'jpg', 'jpeg', 'gif'];
+              var if_img = false;
+              var img_types = ['png', 'jpg', 'jpeg', 'gif'];
 
-    					if (img_types.indexOf(ext.toLowerCase()) > -1){
-    						if_img = true;
-    					}
+              if (img_types.indexOf(ext.toLowerCase()) > -1){
+                if_img = true;
+              }
 
-    					messages[i].if_img = if_img;
-    				}
+              messages[i].if_img = if_img;
+            }
 
-    				$scope.messages = messages;
-    				console.log(messages);
-    			}
-    		);
+            $scope.messages = messages;
+          }
+        );
+      }
 
-    		$scope.create_message = function(ufiles) {
-    			if (!$scope.send_to) {
-    				$scope.errors = 'No user has been selected.'
-    				return;
-    			}
-    			
-    			if (!$scope.text && !ufiles.length) {
-    				$scope.errors = 'Nothing to send to user (no text/image to upload)';
-    				return;
-    			}
+      $scope.create_message = function(ufiles) {
+        if (!$scope.send_to) {
+          $scope.errors = 'No user has been selected.'
+          return;
+        }
 
-    			var data = {
-    				send_by : $localStorage.username,
-    				send_to : $scope.send_to,
-    			};
+        if (!$scope.text && !ufiles.length) {
+          $scope.errors = 'Nothing to send to user (no text/image to upload)';
+          return;
+        }
 
-    			if ($scope.text) {
-    				data.text = $scope.text;
+        var data = {
+          send_by : $localStorage.username,
+          send_to : $scope.send_to,
+        };
 
-    				Message.save({}, data).$promise.then(function(data) {
-    					$scope.messages.push(data);
+        if ($scope.text) {
+          data.text = $scope.text;
 
-    				}, function(error){
-    					$scope.errors = error;
-    				});
-    			}
-    			// bugs (uploads last file multiple times)
-    			else if (ufiles.length) {
-	    			for (var i=0; i < ufiles.length; i++) {
-		    			Upload.upload({
-				            url: 'http://localhost:8000/api/messages/',
-				            data: {
-				            	ufile: ufiles[i],
-				            	send_by : data['send_by'],
-				            	send_to : data['send_to']
-				            }
-				        })
-				        .then(
-				        	function (resp) {
-				            	$scope.messages.push(resp.data);
-				        	},
-					        function (resp) {
-					            $scope.errors = resp.data;
-					        }/*, 
-					        function (evt) {
-					        	console.log(evt);
-					            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-					            console.log('progress: ' + 
-					            	progressPercentage + '% ' + evt.config.data.ufile.name);
-					        }*/
-				        );
-				    }
-    			}
+          Message.save({}, data).$promise.then(function(data) {
+            $scope.messages.push(data);
 
-    			$scope.send_by = '';
-    			$scope.send_to = '';
-    			$scope.text = '';
-    			$scope.ufiles = null;
-    			$scope.errors = '';
-    		}
-		}
-	]
+          }, function(error){
+            $scope.errors = error;
+          });
+        }
+        // bugs (uploads last file multiple times)
+        else if (ufiles.length) {
+          for (var i=0; i < ufiles.length; i++) {
+            Upload.upload({
+              url: 'http://localhost:8000/api/messages/',
+              data: {
+                ufile: ufiles[i],
+                send_by : data['send_by'],
+                send_to : data['send_to']
+              }
+            }).then(
+              function (resp) {
+                $scope.messages.push(resp.data);
+              },
+              function (resp) {
+                $scope.errors = resp.data;
+              }/*,
+              function (evt) {
+                console.log(evt);
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                console.log('progress: ' + progressPercentage + '% ' + evt.config.data.ufile.name);
+              }*/
+            );
+          }
+        }
+
+        $scope.send_by = '';
+        $scope.send_to = '';
+        $scope.text = '';
+        $scope.ufiles = null;
+        $scope.errors = '';
+      }
+    }
+  ]
 );
