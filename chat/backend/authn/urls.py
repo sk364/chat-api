67017f8a207b1d9509f97786c633be840eb03d95
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-07-09 00:12:03 +0530

    Fixed typo

--- chat/backend/authn/urls.py
+++ chat/backend/authn/urls.py
@@ -6,6 +6,6 @@ from .views import google_login, change_password, change_username
 urlpatterns = [
     url(r'^glogin/$', google_login),
     url(r'^login/$', obtain_jwt_token),
-    url(r'^change_password/$', change_username),
+    url(r'^change_password/$', change_password),
     url(r'^change_username/$', change_username),
 ]
