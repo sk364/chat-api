d21c206b6b1c109de78e67e26752bbb523f2c3c1
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-07-09 00:14:02 +0530

    Updated post parameter

--- chat/frontend/public/components/myapp/services/change_pwd.service.js
+++ chat/frontend/public/components/myapp/services/change_pwd.service.js
@@ -1,12 +1,12 @@
 myapp
 	.factory('ChangePwd', function($resource) {
 		return $resource(
-			'http://localhost:8000/api/auth/change_password/',
+			'http://localhost:8000/api/change_password/',
 			{},
 			{
 				'update' : {
 					method : "PUT",
-					data : {password : '@password', cpassword : '@confirm_password'},
+					data : {password : '@password', confirm_password : '@confirm_password'},
 					headers: {
 						'Content-Type' : 'application/json'
 					}
