27f63d81e9e49733bc08e96367dc4f3173c1de39
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-07-08 13:51:17 +0530

    Fixed indentation

--- chat/frontend/public/app.js
+++ chat/frontend/public/app.js
@@ -2,44 +2,41 @@
 
 var myapp = angular.module('myapp',
 [
-    'appRoutes',
-    'ngStorage',
-    'angular-jwt',
-    'ngFileUpload',
-    'ngResource',
+  'appRoutes',
+  'ngStorage',
+  'angular-jwt',
+  'ngFileUpload',
+  'ngResource',
 ])
 .run(function($rootScope, authManager, $window) {
-	authManager.checkAuthOnRefresh();
-	$rootScope.$on('tokenHasExpired', function() {
-        $window.location.href = '/#!/logout';
-	});
+  authManager.checkAuthOnRefresh();
+  $rootScope.$on('tokenHasExpired', function() {
+    $window.location.href = '/#!/logout';
+  });
 });
 
 myapp
-	.config(['$qProvider', '$httpProvider', '$localStorageProvider',
-		'jwtInterceptorProvider', 'jwtOptionsProvider',
-		function ($qProvider, $httpProvider, $localStorageProvider,
-			jwtInterceptorProvider, jwtOptionsProvider) {
-    		
-    		$qProvider.errorOnUnhandledRejections(false);
+  .config(['$qProvider', '$httpProvider', '$localStorageProvider', 'jwtInterceptorProvider', 'jwtOptionsProvider',
+    function ($qProvider, $httpProvider, $localStorageProvider,
+      jwtInterceptorProvider, jwtOptionsProvider) {
+        $qProvider.errorOnUnhandledRejections(false);
 
-    		jwtOptionsProvider.config({
-			    authPrefix: 'JWT ',
-			    whiteListedDomains: ['localhost'],
-			    tokenGetter: ['options', function(options) {
-	        		// Skip authentication for any requests ending in .html
-	        		if (options && (options.url.substr(options.url.length - 5) == '.html') {
-	          			return null;
-	        		}
-	        		
-	        		var token = localStorage.getItem('ngStorage-token');
-	        		
-	        		if (token)
-	        			token = token.substring(1, token.length-1);
-	        		
-	        		return token;
-      			}]
-		    });
+        jwtOptionsProvider.config({
+          authPrefix: 'JWT ',
+          whiteListedDomains: ['localhost'],
+          tokenGetter: ['options', function(options) {
+            // Skip authentication for any requests ending in .html
+            if (options && options.url.substr(options.url.length - 5) == '.html') {
+              return null;
+            }
 
-			$httpProvider.interceptors.push('jwtInterceptor');
-	}]);
+            var token = localStorage.getItem('ngStorage-token');
+	        		
+            if (token)
+              token = token.substring(1, token.length-1);
+              return token;
+          }]
+        });
+
+    $httpProvider.interceptors.push('jwtInterceptor');
+}]);
