cddfd37c2d8c081417f7b719ffebb47dcd3f4ab4
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-08-14 20:20:47 +0530

    Added conversations api endpoint

--- chat/backend/main/urls.py
+++ chat/backend/main/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 
-from .views import MessageAPIView, UserAPIView
+from .views import MessageAPIView, UserAPIView, ConversationsAPIView
 
 messages = MessageAPIView.as_view()
 users = UserAPIView.as_view()
+conversations = ConversationsAPIView.as_view()
 
 urlpatterns = [
     url(r'^users/$', users),
+    url(r'^conversations/$', conversations),
     url(r'^messages/$', messages),
-    url(r'^messages/(?P<username>.*)/$', messages)
+    url(r'^messages/(?P<username>.*)/$', messages),
 ]
--- chat/backend/main/views.py
+++ chat/backend/main/views.py
@@ -50,3 +50,46 @@ class UserAPIView(APIView):
         response_data = user_serializer.data
 
         return Response(response_data)
+
+
+class ConversationsAPIView(APIView):
+    """An API View to get all conversations"""
+
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (JSONWebTokenAuthentication, )
+
+    def is_message_in_list(self, message_list, message):
+        for msg in message_list:
+            if msg.send_by == message.send_by and msg.send_to == message.send_to:
+                return True
+
+        return False
+
+    def serialize_messages(self, messages, auth_user):
+        serialized_messages = []
+        for message in messages:
+            message_dict = {
+                'created_at': message.created_at,
+                'text': message.text,
+                'ufile_name': message.ufile.name if message.ufile else None,
+            }
+            if message.send_by == auth_user:
+                message_dict['user'] = message.send_to.username
+            else:
+                message_dict['user'] = message.send_by.username
+
+            serialized_messages.append(message_dict)
+
+        return serialized_messages
+
+    def get(self, request):
+        user = request.user
+        messages = Message.objects.filter(Q(send_by=user) | Q(send_to=user)).order_by('-created_at')
+
+        first_messages = []
+        for message in messages:
+            if not self.is_message_in_list(first_messages, message):
+                first_messages.append(message)
+
+        serialized_messages = self.serialize_messages(first_messages, user)
+        return Response(serialized_messages)
