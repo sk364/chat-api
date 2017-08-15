d3e67f260835ab21480c0cfc8a63f60e01c129b4
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-08-16 00:00:07 +0530

    Refactored MessageController, added conversations, added update read status

--- chat/frontend/public/components/myapp/controllers/myapp.control.js
+++ chat/frontend/public/components/myapp/controllers/myapp.control.js
@@ -1,103 +1,150 @@
-myapp
-  .controller('MessageController', ["$scope", "$window", "$stateParams", "$localStorage", "Message", "Upload", "User", "socket",
-    function($scope, $window, $stateParams, $localStorage, Message, Upload, User, socket) {
-      if (!('token' in $localStorage)) {
-        $window.location.href = '/#/login';
-      }
+(function() {
+  'use strict';
 
-      $scope.send_to = $stateParams.username;
-      $scope.text = '';
-      $scope.users = [];
-      $scope.active_users = [];
+  myapp.controller('MessageController', MessageController);
+  MessageController.$inject = ["$scope", "$window", "$stateParams", "$localStorage", "Message", "Upload", "User",
+                               "Conversation", "UpdateReadStatus", "socket"];
 
-      $scope.init = function() {
-        socket.init($localStorage.username);
+  function MessageController($scope, $window, $stateParams, $localStorage, Message, Upload, User, Conversation, UpdateReadStatus, socket) {
 
-        socket.on('get:message', $localStorage.username, function(data) {
-          $scope.messages.push(data);
-        });
-
-        var data = {};
-        if ($scope.send_to) {
-          data = { username: $scope.send_to }
-        }
-
-        Message.query(data).$promise.then(function(messages) {
-          $scope.messages = messages;
-        });
-
-        User.query().$promise.then(function(users) {
-          $scope.users = users;
-        });
-      }
-
-      $scope.onSelect = function($item, $model, $value) {
-        window.location.href = '/#/' + $value;
-      }
-
-      $scope.create_message = function(ufiles) {
-        if (!$scope.send_to) {
-          $scope.errors = 'No user has been selected.'
-          return;
-        }
-
-        if (!$scope.text && !ufiles.length) {
-          $scope.errors = 'Nothing to send to user (no text/image to upload)';
-          return;
-        }
-
-        var data = {
-          send_by : $localStorage.username,
-          send_to : $scope.send_to,
-        };
-
-        if ($scope.text) {
-          data.text = $scope.text;
-
-          Message.save({}, data).$promise.then(function(message) {
-            $scope.messages.push(message);
-
-            if (message.send_to !== $localStorage.username) {
-              socket.emit('send:message', message, $localStorage.username);
-            }
-
-          }, function(error){
-            $scope.errors = error;
-          });
-        }
-        else if (ufiles.length) {
-          for (var i=0; i < ufiles.length; i++) {
-            Upload.upload({
-              url: 'http://localhost:8000/api/messages/',
-              data: {
-                ufile: ufiles[i],
-                send_by : data['send_by'],
-                send_to : data['send_to']
-              }
-            }).then(
-              function (resp) {
-                $scope.messages.push(resp.data);
-
-                if (resp.data.send_to !== $localStorage.username)
-                  socket.emit('send:message', resp.data, $localStorage.username);
-              },
-              function (resp) {
-                $scope.errors = resp.data;
-              }/*,
-              function (evt) {
-                console.log(evt);
-                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                console.log('progress: ' + progressPercentage + '% ' + evt.config.data.ufile.name);
-              }*/
-            );
-          }
-        }
-
-        $scope.send_by = '';
-        $scope.text = '';
-        $scope.ufiles = null;
-        $scope.errors = '';
-      }
+    if (!('token' in $localStorage)) {
+      $window.location.href = '/#/login';
     }
-  ]
-);
+
+    $scope.send_to = $stateParams.username;
+    $scope.text = '';
+    $scope.users = [];
+    $scope.active_users = [];
+    $scope.conversations = [];
+
+    $scope.init = function() {
+      var data = {};
+      if ($scope.send_to) {
+        data = { username: $scope.send_to }
+      }
+
+      socket.init($localStorage.username);
+      socket.on('get:message', $localStorage.username, function(data) {
+        if (data.send_by === $scope.send_to) {
+          $scope.messages.push(data);
+        }
+        $scope.updateConversations(data);
+      });
+
+      Message.query(data).$promise.then(function(messages) {
+        $scope.messages = messages;
+      });
+
+      User.query().$promise.then(function(users) {
+        $scope.users = users;
+      });
+
+      Conversation.query().$promise.then(function(conversations) {
+        $scope.conversations = conversations;
+      });
+
+      data = {'username': $scope.send_to, 'timestamp': new Date()};
+      UpdateReadStatus.update({}, data).$promise.then(function(data) {
+        if (data.success) {
+          // idk what to do here.
+        }
+      })
+    }
+
+    $scope.updateConversations = function(message) {
+      var updated = false;
+      $scope.conversations.map(function(msg, index) {
+        if (message.send_by === msg.user) {
+          msg.text = message.text;
+          msg.ufile_name = message.ufile_name;
+          msg.created_at = message.created_at;
+          updated = true;
+        }
+      });
+
+      if (!updated) {
+        $scope.conversations.push({
+          user: message.send_by,
+          text: message.text,
+          ufile_name: message.ufile_name,
+          created_at: message.created_at
+        });
+      }
+
+      $scope.conversations.sort(function(msg1, msg2) {
+        return new Date(msg1.created_at) - new Date(msg2.created_at);
+      });
+    }
+
+    $scope.onSelect = function($item, $model, $value) {
+      window.location.href = '/#/' + $value;
+    }
+
+    $scope.create_message = function(ufiles) {
+      if (!$scope.send_to) {
+        $scope.errors = 'No user has been selected.'
+        return;
+      }
+
+      if (!$scope.text && !ufiles.length) {
+        $scope.errors = 'Nothing to send to user (no text/image to upload)';
+        return;
+      }
+
+      var data = {
+        send_by : $localStorage.username,
+        send_to : $scope.send_to,
+      };
+
+      if ($scope.text) {
+        data.text = $scope.text;
+
+        Message.save({}, data).$promise.then(function(message) {
+          $scope.messages.push(message);
+
+          if (message.send_to !== $localStorage.username) {
+            socket.emit('send:message', message, $localStorage.username);
+          }
+
+          $scope.updateConversations(message);
+        }, function(error){
+          $scope.errors = error;
+        });
+      }
+      else if (ufiles.length) {
+        for (var i=0; i < ufiles.length; i++) {
+          Upload.upload({
+            url: 'http://localhost:8000/api/messages/',
+            data: {
+              ufile: ufiles[i],
+              send_by : data['send_by'],
+              send_to : data['send_to']
+            }
+          }).then(
+            function (resp) {
+              $scope.messages.push(resp.data);
+
+              if (resp.data.send_to !== $localStorage.username)
+                socket.emit('send:message', resp.data, $localStorage.username);
+
+              $scope.updateConversations(resp.data);
+            },
+            function (resp) {
+              $scope.errors = resp.data;
+            }/*,
+            function (evt) {
+              console.log(evt);
+              var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+              console.log('progress: ' + progressPercentage + '% ' + evt.config.data.ufile.name);
+            }*/
+          );
+        }
+      }
+
+      $scope.send_by = '';
+      $scope.text = '';
+      $scope.ufiles = null;
+      $scope.errors = '';
+    }
+  }
+})();
--- chat/frontend/public/components/myapp/services/message.service.js
+++ chat/frontend/public/components/myapp/services/message.service.js
@@ -1,48 +1,84 @@
-myapp
-	.factory('Message', function($resource) {
-		return $resource(
-			'http://localhost:8000/api/messages/:username/',
-			{},
-			{
-				'query': {
-					method : "GET",
-					headers : {
-						'Content-Type' : 'application/json'
-					},
-					isArray: true
+myapp.factory('Message', function($resource) {
+	return $resource(
+		'http://localhost:8000/api/messages/:username/',
+		{},
+		{
+			'query': {
+				method : "GET",
+				headers : {
+					'Content-Type' : 'application/json'
 				},
-				'save': {
-					method : "POST",
-					headers : {
-						'Content-Type' : 'application/json'
-					}
-				},
-				'fsave': {
-					method: "POST",
-                    headers: {
-                        'Content-Type' : undefined,
-                        enctype : 'multipart/form-data'
-                    },
-                    transformRequest: function (data) {
-                        var fd = new FormData();
-                        angular.forEach(data, function(value, key) {
-                            if (value instanceof File)
-                                fd.append(key, value);
-                            else if (value !== null &&  typeof value === 'object'){
-                                fd.append(key, JSON.stringify(value)); 
-                            } 
-                            else {
-                                fd.append(key, value);
-                            }
-                        });
-                        
-                        return fd;
-                    },
+				isArray: true
+			},
+			'save': {
+				method : "POST",
+				headers : {
+					'Content-Type' : 'application/json'
 				}
 			},
-			{
-				stripTrailingSlashes: false
+			'fsave': {
+				method: "POST",
+                headers: {
+                    'Content-Type' : undefined,
+                    enctype : 'multipart/form-data'
+                },
+                transformRequest: function (data) {
+                    var fd = new FormData();
+                    angular.forEach(data, function(value, key) {
+                        if (value instanceof File)
+                            fd.append(key, value);
+                        else if (value !== null &&  typeof value === 'object'){
+                            fd.append(key, JSON.stringify(value)); 
+                        } 
+                        else {
+                            fd.append(key, value);
+                        }
+                    });
+                    
+                    return fd;
+                },
 			}
-		);
-	}
-);
+		},
+		{
+			stripTrailingSlashes: false
+		}
+    )
+});
+
+myapp.factory('Conversation', function($resource) {
+    return $resource(
+        'http://localhost:8000/api/conversations/',
+        {},
+        {
+            'query': {
+                method : "GET",
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                isArray: true
+            }
+        },
+        {
+            stripTrailingSlashes: false
+        }
+    )
+});
+
+myapp.factory('UpdateReadStatus', function($resource) {
+    return $resource(
+        'http://localhost:8000/api/update_read_status/',
+        {},
+        {
+            'update': {
+                method : "PUT",
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                isArray: false
+            }
+        },
+        {
+            stripTrailingSlashes: false
+        }
+    )
+});
