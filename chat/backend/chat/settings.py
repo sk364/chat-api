b410a9b6c0e9323bcbac0b30ee95e4ee165f1700
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-08-29 01:47:55 +0530

    Added whitenoise

--- chat/backend/chat/settings.py
+++ chat/backend/chat/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -182,6 +183,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGGING = {
     'version': 1,
